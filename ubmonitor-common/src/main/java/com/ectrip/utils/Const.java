eca19fb1d5e9027d2a62efc615d1eaa8b9773cfe
huangxinguang <hxguang911219@163.com>
2017-04-19 16:09:21 +0800

    增加分页插件

---
+++ ubmonitor-common/src/main/java/com/ectrip/utils/Const.java
@@ -0,0 +1,13 @@
+package com.ectrip.utils;
+
+import org.springframework.context.ApplicationContext;
+/**
+ * 项目名称：
+ * @author:fh qq313596790[青苔]
+ * 修改日期：2015/11/2
+*/
+public class Const {
+
+	
+	
+}
