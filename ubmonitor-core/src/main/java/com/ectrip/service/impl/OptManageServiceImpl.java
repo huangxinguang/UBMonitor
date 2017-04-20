f10b82422595d4f2978254ec16fc59a50406a96a
huangxinguang <hxguang911219@163.com>
2017-04-20 17:34:55 +0800

    删除userAgengUtil解析工具，增加userAgentUtils.jar

--- ubmonitor-core/src/main/java/com/ectrip/service/impl/OptManageServiceImpl.java
+++ ubmonitor-core/src/main/java/com/ectrip/service/impl/OptManageServiceImpl.java
@@ -2,17 +2,23 @@ package com.ectrip.service.impl;
 
 import com.ectrip.dao.OptEnvironmentDAO;
 import com.ectrip.dao.OptRecordDAO;
+import com.ectrip.model.OptEnvironment;
 import com.ectrip.model.OptRecord;
 import com.ectrip.service.OptManageService;
-import com.ectrip.utils.UserAgentUtil;
-import cz.mallat.uasparser.UserAgentInfo;
+import com.ectrip.utils.NetUtil;
+import nl.bitwalker.useragentutils.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by huangxinguang on 2017/4/20 下午2:24.
  * </p>
  * Desc:
  */
+@Service
 public class OptManageServiceImpl implements OptManageService {
 
     @Autowired
@@ -21,10 +27,58 @@ public class OptManageServiceImpl implements OptManageService {
     @Autowired
     private OptRecordDAO optRecordDAO;
 
-    @Override
-    public void saveOptAndEnv(String hostName, String userAgent, String accept, String acceptLanguage, String acceptEncoding, String keepAlive, String cookies, String sessionId, OptRecord optRecord) {
-        UserAgentInfo userAgentInfo = UserAgentUtil.parse(userAgent);
+    @Autowired
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
+    @Override
+    public void saveOptAndEnv(final HttpServletRequest request, final OptRecord optRecord) {
+
+        // 使用线程池多线程处理
+        threadPoolTaskExecutor.execute(new Runnable() {
+            public void run() {
+                try {
+                    String sessionId = request.getRequestedSessionId();
+                    String operators = request.getRemoteUser();
+                    String url = request.getRequestURL().toString();
+                    String ip = request.getRemoteAddr();
+                    String mac = NetUtil.getMACAddress(ip);
+                    optRecord.setReqUrl(url);
+                    optRecord.setSessionId(sessionId);
+
+                    String userAgent = request.getHeader("User-Agent");
+
+
+                    UserAgent userAgentInfo = UserAgent.parseUserAgentString(userAgent);
+                    Browser browser = userAgentInfo.getBrowser();
+                    Version version = userAgentInfo.getBrowserVersion();
+
+                    OperatingSystem operatingSystem = userAgentInfo.getOperatingSystem();
+
+
+                    OptEnvironment env = new OptEnvironment();
+                    env.setIp(ip);
+                    env.setMac(mac);
+                    env.setComputerName(operatingSystem.getDeviceType().getName());
+                    env.setOperators(operators);
+                    env.setBrowser(browser.getName());
+                    env.setBrowserVersion(version.getVersion());
+                    env.setOs(operatingSystem.getName());
+                    env.setOsVersion(operatingSystem.getDeviceType().getName());
+                    env.setManufacturer(operatingSystem.getManufacturer().getName());
+                    env.setPhoneModel(NetUtil.getPhoneModel(userAgent));
+                    env.setDeviceId(operatingSystem.getId()+"");
+
+                    optEnvironmentDAO.save(env);
+
+
+                    optRecord.setEnvId(env.getId());
+                    optRecordDAO.save(optRecord);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     /**
---
+++ ubmonitor-core/src/test/com/ectip/UserAgentUtilsTest.java
@@ -0,0 +1,38 @@
+package com.ectip;
+
+import nl.bitwalker.useragentutils.UserAgent;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by huangxinguang on 2017/4/20 下午3:58.
+ * </p>
+ * Desc:
+ */
+public class UserAgentUtilsTest {
+
+    @Test
+    public void testMobile() {
+
+        String userAgent = "Huawei U8800    Android 2.3.3   Baidu 2.3   Mozilla/5.0 (Linux; U; Android 2.3.5; zh-cn; U8800 Build/HuaweiU8800) AppleWebKit/530.17 (KHTML, like Gecko)";
+        UserAgent userAgentInfo = UserAgent.parseUserAgentString(userAgent);
+
+    }
+
+    @Test
+    public void testMobileType() {
+        Pattern pattern = Pattern.compile(";\\s?(\\S*?\\s?\\S*?)\\s?(Build)?/");
+        //String userAgent = "Huawei U8800    Android 2.3.3   UC 8.7  Mozilla/5.0 (Linux; U; Android 2.3.5; zh-cn; U8800 Build/HuaweiU8800) UC AppleWebKit/534.31 (KHTML, like Gecko) Mobile Safari/534.31";
+        String userAgent = "Mozilla/5.0 (Linux; U; Android 4.3; zh-cn; R8007 Build/JLS36C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
+        Matcher matcher = pattern.matcher(userAgent);
+        String model = null;
+        if (matcher.find()) {
+            model = matcher.group(1).trim();
+            //log.debug("通过userAgent解析出机型：" + model);
+        }
+    }
+}
