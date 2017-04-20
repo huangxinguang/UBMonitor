a78dec01a366b1f21c335e893e7989bd7147272f
aishenxiao <aijin.xiao@ectrip.com>
2017-04-20 15:10:47 +0800

    新增用户环境信息DAO接口和Mapper映射文件

---
+++ ubmonitor-core/src/main/java/com/ectrip/dao/OptEnvironmentDAO.java
@@ -0,0 +1,13 @@
+package com.ectrip.dao;
+
+import com.ectrip.model.OptEnvironment;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/20 0020.
+ *
+ */
+public interface OptEnvironmentDAO {
+    public List<OptEnvironment> getOptRecordInfo();
+}
