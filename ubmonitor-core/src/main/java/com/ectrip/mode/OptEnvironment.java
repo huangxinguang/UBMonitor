ad27509dc023b33886ae95bf708ababa8d331544
huangxinguang <hxguang911219@163.com>
2017-04-20 09:23:32 +0800

    添加model

---
+++ ubmonitor-core/src/main/java/com/ectrip/mode/OptEnvironment.java
@@ -0,0 +1,225 @@
+package com.ectrip.mode;
+
+/**
+ * Created by huangxinguang on 2017/4/20 上午9:14.
+ * </p>
+ * Desc:
+ */
+public class OptEnvironment {
+    private Integer id;
+    private String ip;
+    private String clientIp;
+    private String mac;
+    private String imei;
+    private String operators;
+    private String manufacturer;
+    private String phoneModel;
+    private String computerName;
+    private String os;
+    private String osVersion;
+    private String resolution;
+    private String font;
+    private String fontSize;
+    private String browser;
+    private String browserVersion;
+    private String appName;
+    private String appType;
+    private String appVersion;
+    private String longitude;
+    private String latitude;
+    private String internetType;
+    private String isEmulator;
+    private String deviceId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getOperators() {
+        return operators;
+    }
+
+    public void setOperators(String operators) {
+        this.operators = operators;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getPhoneModel() {
+        return phoneModel;
+    }
+
+    public void setPhoneModel(String phoneModel) {
+        this.phoneModel = phoneModel;
+    }
+
+    public String getComputerName() {
+        return computerName;
+    }
+
+    public void setComputerName(String computerName) {
+        this.computerName = computerName;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getOsVersion() {
+        return osVersion;
+    }
+
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getFont() {
+        return font;
+    }
+
+    public void setFont(String font) {
+        this.font = font;
+    }
+
+    public String getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(String fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getBrowserVersion() {
+        return browserVersion;
+    }
+
+    public void setBrowserVersion(String browserVersion) {
+        this.browserVersion = browserVersion;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getAppType() {
+        return appType;
+    }
+
+    public void setAppType(String appType) {
+        this.appType = appType;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getInternetType() {
+        return internetType;
+    }
+
+    public void setInternetType(String internetType) {
+        this.internetType = internetType;
+    }
+
+    public String getIsEmulator() {
+        return isEmulator;
+    }
+
+    public void setIsEmulator(String isEmulator) {
+        this.isEmulator = isEmulator;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+}
--- ubmonitor-core/src/main/java/com/ectrip/mode/OptRecord.java
+++ ubmonitor-core/src/main/java/com/ectrip/mode/OptRecord.java
@@ -6,4 +6,139 @@ package com.ectrip.mode;
  * Desc:
  */
 public class OptRecord {
+    private Integer id;
+    private String sysCode;
+    private String channelCode;
+    private String channelName;
+    private String terminalName;
+    private String sessionId;
+    private String reqParams;
+    private String reqUrl;
+    private String userId;
+    private String sceneNo;
+    private String reqMethodName;
+    private String optBrief;
+    private String optTime;
+    private String optDescription;
+    private Integer envId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSysCode() {
+        return sysCode;
+    }
+
+    public void setSysCode(String sysCode) {
+        this.sysCode = sysCode;
+    }
+
+    public String getChannelCode() {
+        return channelCode;
+    }
+
+    public void setChannelCode(String channelCode) {
+        this.channelCode = channelCode;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public String getTerminalName() {
+        return terminalName;
+    }
+
+    public void setTerminalName(String terminalName) {
+        this.terminalName = terminalName;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getReqParams() {
+        return reqParams;
+    }
+
+    public void setReqParams(String reqParams) {
+        this.reqParams = reqParams;
+    }
+
+    public String getReqUrl() {
+        return reqUrl;
+    }
+
+    public void setReqUrl(String reqUrl) {
+        this.reqUrl = reqUrl;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSceneNo() {
+        return sceneNo;
+    }
+
+    public void setSceneNo(String sceneNo) {
+        this.sceneNo = sceneNo;
+    }
+
+    public String getReqMethodName() {
+        return reqMethodName;
+    }
+
+    public void setReqMethodName(String reqMethodName) {
+        this.reqMethodName = reqMethodName;
+    }
+
+    public String getOptBrief() {
+        return optBrief;
+    }
+
+    public void setOptBrief(String optBrief) {
+        this.optBrief = optBrief;
+    }
+
+    public String getOptTime() {
+        return optTime;
+    }
+
+    public void setOptTime(String optTime) {
+        this.optTime = optTime;
+    }
+
+    public String getOptDescription() {
+        return optDescription;
+    }
+
+    public void setOptDescription(String optDescription) {
+        this.optDescription = optDescription;
+    }
+
+    public Integer getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Integer envId) {
+        this.envId = envId;
+    }
 }
